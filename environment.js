be30514c21c29aac51186df1a474b8df54b546f1
TRI <tariq@advency.fr>
2021-12-30 17:50:33 +0100

    Environment.js

---
+++ environment.js
@@ -0,0 +1,10 @@
+const environment = {
+  urlApi: "https://server.embedgaming.com/play",
+  token: "<YOUR API KEY>", //https://embedgaming.com
+  rtp: 99,
+  port: 8083
+}
+
+module.exports = {
+  environment
+}
